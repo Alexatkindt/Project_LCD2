bfb6d288c73a43da80030d3a735c60e0fced1e27
alexa <alexa@JUAN>
2023-12-11 22:47:32 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace Project_LCD2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SerialCommunicationManager serialManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeSerialManager();
+        }
+
+        private void InitializeSerialManager()
+        {
+            serialManager = new SerieleCommunicatie("COM8", 9600); // Replace with your actual COM port and baud rate
+            serialManager.DataReceived += SerialManager_DataReceived;
+            serialManager.Open();
+        }
+
+        private void SerialManager_DataReceived(object sender, string receivedData)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                // Update UI or perform actions based on received data
+                // For example, display the received data in a TextBox
+                textBoxReceivedData.Text = receivedData;
+            });
+        }
+
+        private void Button_SendData_Click(object sender, RoutedEventArgs e)
+        {
+            // Send data to Arduino when the button is clicked
+            serialManager.SendData("Hello, Arduino!");
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            // Close the serial port when the window is closed
+            serialManager.Close();
+
+            base.OnClosed(e);
+        }
+
+        private void delete(object sender, RoutedEventArgs e)
+        {
+            serialManager.SendData("reset");
+        }
+    }
+}
---
+++ SerieleCommunicatie.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Project_LCD2
+{
+    public class SerieleCommunicatie
+    {
+        private SerialPort serialPort;
+
+        public event EventHandler<string> DataReceived;
+
+        public SerieleCommunicatie(string portName, int baudRate)
+        {
+            serialPort = new SerialPort(portName, baudRate);
+            serialPort.DataReceived += SerialPort_DataReceived;
+        }
+
+        public void Open()
+        {
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening serial port: {ex.Message}");
+            }
+        }
+
+        public void Close()
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        public void SendData(string data)
+        {
+            try
+            {
+                serialPort.WriteLine(data);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error sending data: {ex.Message}");
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string receivedData = serialPort.ReadLine();
+                DataReceived?.Invoke(this, receivedData);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error processing received data: {ex.Message}");
+            }
+        }
+    }
+}
+
