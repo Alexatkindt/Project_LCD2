1070eb64768b1ea901d975120b969634fbae8a97
Alexa 't Kindt <r0981937@student.vives.be>
2023-12-12 14:17:14 +0100

    commentaar toegevoegd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Ports;
+using System.Windows.Markup;
 
 namespace Project_LCD2
 {
@@ -17,47 +18,64 @@ namespace Project_LCD2
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Instance of the class responsible for serial communication
         private SerieleCommunicatie serialManager;
 
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            // Initialiseer seriele communicatie
             InitializeSerialManager();
         }
 
+        
         private void InitializeSerialManager()
         {
-            serialManager = new SerieleCommunicatie("COM8", 9600); 
+            // maak een serielecommunicatie aan met com8 en baudrate 9600
+            serialManager = new SerieleCommunicatie("COM8", 9600);
+
+            //wanneer de data toegekomen is roep de methode SerialPort_DataReceived.
             serialManager.DataReceived += SerialManager_DataReceived;
+
+            // Open de serial port
             serialManager.Open();
         }
 
+        
         private void SerialManager_DataReceived(object sender, string receivedData)
         {
+            // Update the UI met de ontvangen data
             Dispatcher.Invoke(() =>
             {
-               textBoxReceivedData.Text = receivedData;
+                textBoxReceivedData.Text = receivedData;
             });
         }
-        
-        //Hello arduino sturen naar LCD
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Send "Hello, Arduino!" to the Arduino via de seriele poort
             serialManager.SendData("Hello, Arduino!");
         }
 
         protected override void OnClosed(EventArgs e)
         {
-            // Close the serial port when the window is closed
+            // sluit poort wanneer het venster sluit
             serialManager.Close();
 
+            // Call the base class implementation
             base.OnClosed(e);
         }
 
-        //LCD reset
+        
         private void delete(object sender, RoutedEventArgs e)
         {
+            // stuur reset naar seeeduino via seriele poort.
             serialManager.SendData("reset");
         }
     }
 }
+
--- SerieleCommunicatie.cs
+++ SerieleCommunicatie.cs
@@ -1,41 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Project_LCD2
 {
+    
     public class SerieleCommunicatie
     {
         private SerialPort serialPort;
 
+        
         public event EventHandler<string> DataReceived;
 
+        
         public SerieleCommunicatie(string portName, int baudRate)
         {
+            // Initialiseert poort met poortnaam en baudrate
             serialPort = new SerialPort(portName, baudRate);
+
+            // wanneer de data toegekomen is roep de methode SerialPort_DataReceived.
             serialPort.DataReceived += SerialPort_DataReceived;
         }
 
+        /// <summary>
+        /// Opens the serial port for communication.
+        /// </summary>
         public void Open()
         {
             try
             {
+                //probeer seriele poort te openen.
                 serialPort.Open();
             }
             catch (Exception ex)
             {
+                // toon error als het openen van serielepoort fout loopt.
                 MessageBox.Show($"Error opening serial port: {ex.Message}");
             }
         }
 
+        /// <summary>
+        /// Closes the serial port if it is open.
+        /// </summary>
         public void Close()
         {
             if (serialPort.IsOpen)
             {
+                // sluit poort als de poort open is
                 serialPort.Close();
             }
         }
@@ -44,10 +55,12 @@ namespace Project_LCD2
         {
             try
             {
+                // stuur data door naar de serieleport.
                 serialPort.WriteLine(data);
             }
             catch (Exception ex)
             {
+                // toon error als data failed.
                 MessageBox.Show($"Error sending data: {ex.Message}");
             }
         }
@@ -56,14 +69,17 @@ namespace Project_LCD2
         {
             try
             {
+                // Read a line of data from the serial port.
                 string receivedData = serialPort.ReadLine();
+
+                // Invoke the DataReceived event, passing the received data.
                 DataReceived?.Invoke(this, receivedData);
             }
             catch (Exception ex)
             {
+                // toon messagebox als een error optreedt
                 MessageBox.Show($"Error processing received data: {ex.Message}");
             }
         }
     }
 }
-
